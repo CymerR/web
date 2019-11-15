5542f7fab3491d72bb7602b08334a721bc385bea
CymerR <57258432+CymerR@users.noreply.github.com>
2019-11-15 22:33:00 +0300

    Add files via upload

---
+++ JS/js/main.js
@@ -0,0 +1,38 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+
+let WIDTH = window.innerWidth;
+let HEIGHT = window.innerHeight;
+
+let MBox = new Box(WIDTH/2, HEIGHT/2);
+
+let velocityX = 5;
+let velocityY = 5;
+//functions
+function clear(){
+  ctx.fillStyle = "red";
+  ctx.fillRect(0,0,WIDTH,HEIGHT);
+}
+
+function update(){
+    MBox.x += velocityX;
+    MBox.y += velocityY;
+    if (MBox.x >= WIDTH)
+      velocityX = -velocityX;
+    if (MBox.y >= HEIGHT)
+      velocityX = -velocityY;
+}
+
+function show(){
+  ctx.fillStyle = "black";
+  ctx.fillRect(MBox.x, MBox.y, 50, 50);
+}
+
+function loop(){
+  while(true){
+    clear();
+    update();
+    show();
+  }
+}
+loop();
