e3ca38f27f417ba6bec0ed7aaee88f26497db1b9
CymerR <57258432+CymerR@users.noreply.github.com>
2019-11-24 10:39:39 +0300

    under develop on phys :')

--- JS/js/main.js
+++ JS/js/main.js
@@ -1,34 +1,69 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
+let x = 50, y = 110;
+
+let dx = 6,
+    dy = 9,
+    mass = 1,
+    radius = 40,
+    k = 1; // px \ frame
 
 let WIDTH = window.innerWidth;
 let HEIGHT = window.innerHeight;
 
-let MBox = new Box(WIDTH/2, HEIGHT/2);
+//theory
 
-let x = 500, y = 300;
+let frx  = 1,
+    fry  = 1,
+    ax = 0,
+    ay = 1;
+/*
 
-let velocityX = 5;
-let velocityY = 5;
+    Сила = масса * ускорение -> ускорение = сила / масса
+    Скорость = предыдущая скорость + ускорение ( на время, но здесь это каждый фрейм)
+
+*/
+//init
+canvas.height = 500
+canvas.width = 520 ;
+
+dx = 20;
+dy = 10;
 //functions
 function clear(){
-  ctx.fillStyle = "red";
-  ctx.fillRect(0,0,WIDTH,HEIGHT);
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.height, canvas.width);
 }
 
 function update(){
-    x += velocityX;
-    y += velocityY;
 
+  dx += frx / mass
+  dy += fry / mass
+  // if(x + dx > canvas.width - radius || x + dx < radius)
+  //   dx = - k * dx;
+  // if(y + dy > canvas.height - radius || y + dy < radius)
+  //   dy = - k * dy;
+
+    if(x + dx > canvas.width-radius || x + dx < radius)
+    dx = -dx;
+
+    if(y + dy > canvas.height-radius || y + dy < radius)
+    dy = -dy
+
+
+    x += dx
+    y += dy
 }
 
 function show(){
-  ctx.fillStyle = "green";
-  ctx.fillRect(x, y, 50, 50);
+    ctx.fillStyle = "#29a329";
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.fill();
 }
 
-let timer = setInterval(()=>{
-    clear();
-    update();
-    show();
-  }, 100);
+setInterval(function () {
+  clear();
+  update();
+  show();
+}, 20);
