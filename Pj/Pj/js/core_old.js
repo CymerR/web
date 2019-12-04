e49373b56dc84152ba7702cdeb34d290b351a504
CymerR <57258432+CymerR@users.noreply.github.com>
2019-12-04 21:40:22 +0300

    EEE

--- Pj/Pj/js/core_old.js
+++ Pj/Pj/js/core_old.js
@@ -1,80 +1,110 @@
 const canvas = document.querySelector("#canvas"),
-	  ctx    = canvas.getContext("2d");
+	  	ctx    = canvas.getContext("2d");
+
+let flagGravity = document.querySelector('#checkbox')
 
 
 canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight - 80;
 
+let slider = document.querySelector('#gravity')
 
-class Obj{
+slider.min = 0.1
+slider.max = 10;
+slider.step = 0.05
 
-	constructor(mass, x, y, radius){
+slider.defaultValue = .23;
+
+function Entity(mass, x, y) {
 		this.mass = mass
 		this.x = x
 		this.y = y
-		this.radius = radius
-	}
 
-	update(dx, dy){
-		this.x += dx
-		this.y += dy
-	}
-
-	determine(vAcc){
-		this.ax = vAcc.x
-		this.ay = vAcc.y
-	}
-
+		this.dx = 0
+		this.dy = 0
 }
-
 
 let x  = canvas.width  / 2
 let y  = canvas.height / 2,
-	dy = 0,
-	dx = 8,
-	dy2 = 0,
-	dx2 = 0,
-	k  = 1,
-	dt = 0.15,
-	kuk = 0.3,
-	ky = 0.73,
-	radius = 30,
-	gravity = .23;
+		dx = 1,
+		dy = 4,
+		radius = 30,
+		gravity = .23;
+
+var fi = 0;
+
+
+ball =  new Entity(15, x, y);
+ball.dy = -5
+ball.dx = 5;
+ball.radius = 60
+
+ball2 = new Entity(14, x+20, y - 40);
 
 setInterval(() =>{
 
+ 	var fi = 0;
+	gravity = +slider.value;
+
+	if (flagGravity.checked)
+			gravity = 0;
+
 	//clear
-	ctx.fillStyle = "#000";
-	ctx.clearRect(0,0, canvas.width, canvas.height);
+	clear()
 	//draw
 	ctx.fillStyle = "#fff";
-	ctx.fillRect(x, y, radius, radius)
+	ctx.fillRect(ball.x, ball.y, ball.radius, ball.radius);
 
-	// ctx.fillText("x = " + ball1.x, 40, 30);
-	// ctx.fillText("y = " + ball1.y, 40, 50);
-	//
-	// ctx.fillText("x = " + ball2.x, 80, 80);
-	// ctx.fillText("y = " + ball2.y, 80, 100);
+	fi = Math.atan(Math.abs(ball.dy) /  Math.abs(ball.dx));
 
-	if (x + 50 >= canvas.width  || x <= 0)
-		dx = -dx * k;
-	if (y + 50 >= canvas.height || y <= 0)
-		dy = -dy
-
-	// if (ball2.x + 50 >= canvas.width  || ball2.x <= 0)
-	// 	dx2 = -dx2 * k;
-	// if (ball2.y + 50 >= canvas.height || ball2.y <= 0)
-	// 	dy2 = -dy2 * 0.76
+	ctx.fillStyle = "honeydew";
+	ctx.font = "48px serif"
+	ctx.fillText("φ : " + Math.round(100 * fi) / 100, 40, 70);
 
 
-	x += dx;
-	y += dy;
+	ctx.beginPath();
+	ctx.moveTo(50, 90);
+	ctx.lineTo(Math.sin(fi) * 15, Math.cos(fi) * 15 );
+	ctx.fill();
 
-	dy += gravity
-	//
-	//
-	// ball1.update(dx, dy)
-	// ball2.update(dx2, dy2)
+	//collision tests
+	collisionTester(ball);
+
+		ball.x += ball.dx;
+		ball.y += ball.dy;
+
+		ball.dy += gravity;
 
 
 }, 20)
+
+
+function clear() {
+	ctx.fillStyle = "#000";
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+}
+
+
+function collisionTester(ball) {
+	if (ball.x + ball.radius >= canvas.width  || ball.x <= 0)
+			ball.dx = -ball.dx * 0.9;
+
+	ball.x + ball.radius >= canvas.width ? ball.x = canvas.width- ball.radius : ball.x = ball.x;
+	ball.x <= 0 ? ball.x = 0 : ball.x = ball.x;
+	if (ball.y <= 0)
+			ball.dy =  -ball.dy * 0.7;
+	if (ball.y + ball.radius >= canvas.height)
+			ball.dy = -ball.dy + 2 * gravity;
+	if (ball.y <= 0)
+		ball.y = 0
+	if (ball.y + ball.radius >= canvas.height)
+			ball.y = canvas.height - ball.radius
+}
+canvas.addEventListener("click",(e)=>{
+	ball.dx *= 1.4
+	ball.dy *= 1.4;
+});
+
+canvas.addEventListener("keyDown", (evetn)=> {
+	console.log(evetn.key);
+})
